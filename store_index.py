216657a427302f36d56c8c67d014cc6377b146b6
shilpiagarwal1 <shilpigupta.eng@gmail.com>
2026-04-01 11:55:50 +0530

    Update store_index.py

--- store_index.py
+++ store_index.py
@@ -5,7 +5,7 @@ from langchain.vectorstores import Pinecone
 from dotenv import load_dotenv
 import os
 from pinecone import Pinecone, ServerlessSpec
-
+from langchain_pinecone import PineconeVectorStore
 
 
 load_dotenv()
@@ -14,28 +14,43 @@ PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
 os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
 
 
-extracted_data=load_pdf_file(data='Data/')
+extracted_data=load_pdf_file(data='Data/nutrition/')
 text_chunks=text_split(extracted_data)
 embeddings = download_hugging_face_embeddings()
 
 pc = Pinecone(api_key=PINECONE_API_KEY)
 
-index_name = "medicalbot"
+index_name = "nutrition"
 
 
-pc.create_index(
-    name=index_name,
-    dimension=384, 
-    metric="cosine", 
-    spec=ServerlessSpec(
+
+existing_indexes = [i["name"] for i in pc.list_indexes()]
+
+print(existing_indexes)
+
+if index_name not in existing_indexes:
+    Pinecone.create_index(
+        name=index_name,
+        dimension=384,
+        metric="cosine",
+        spec=ServerlessSpec(
         cloud="aws", 
-        region="us-east-1"
+        region="us-east-1") 
+    )
+    
+# pc.create_index(
+#   name=index_name,
+#    dimension=384, 
+#    metric="cosine", 
+    # spec=ServerlessSpec(
+        # cloud="aws", 
+        # region="us-east-1"
 
-    ) 
-) 
+    # ) 
+# ) 
 
 # Embed each chunk and upsert the embeddings into your Pinecone index.
-docsearch = Pinecone.from_documents(
+docsearch = PineconeVectorStore.from_documents(
     documents=text_chunks,
     index_name=index_name,
     embedding=embeddings, 
